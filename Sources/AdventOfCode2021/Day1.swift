8d74de29a6c29d8be2a6f29fa6f62b5db5c36738
Eli Perkins <eli.j.perkins@gmail.com>
2021-12-01 17:46:08 -0500

    Use Array.windows(ofCount:)

--- Sources/AdventOfCode2021/Day1.swift
+++ Sources/AdventOfCode2021/Day1.swift
@@ -1,25 +1,29 @@
+import Algorithms
+
 public enum Day1 {
     public static func solve(_ input: String) -> Int {
         let values = input.lines.compactMap(Int.init)
-        let pairs = zip(values, values.dropFirst())
-        return pairs.reduce(0) { acc, next in
-            if next.0 < next.1 {
+        return values.windows(ofCount: 2).reduce(0) { acc, next in
+            guard let lhs = next.first, let rhs = next.last else { return acc }
+
+            if lhs < rhs {
                 return acc + 1
             }
+
             return acc
         }
     }
 
     public static func solvePartTwo(_ input: String) -> Int {
         let values = input.lines.compactMap(Int.init)
-        let xs = zip(values, zip(values.dropFirst(), values.dropFirst().dropFirst())).map {
-            $0.0 + $0.1.0 + $0.1.1
-        }
-        let pairs = zip(xs, xs.dropFirst())
-        return pairs.reduce(0) { acc, next in
-            if next.0 < next.1 {
+        let xs = values.windows(ofCount: 3).map { $0.reduce(0, +) }
+        return xs.windows(ofCount: 2).reduce(0) { acc, next in
+            guard let lhs = next.first, let rhs = next.last else { return acc }
+
+            if lhs < rhs {
                 return acc + 1
             }
+
             return acc
         }
     }
